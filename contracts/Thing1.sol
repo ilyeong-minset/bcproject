bb2424356808803c9023c40a188266b49d642b9f
lildata <lildata@users.noreply.github.com>
2020-01-20 11:52:33 +0100

    various dev

--- contracts/Thing1.sol
+++ contracts/Thing1.sol
@@ -27,7 +27,31 @@ contract Thing is
   using SafeMath for uint256;
   using Counters for Counters.Counter;
 
+
+  // FEATURE 2 : token metadata (on-chain)
+  struct Metadata {
+    string name;
+    string picture; //IPFS hash
+    uint256 deposit;
+  }
+
+  mapping(uint256 => Metadata) private metadatas;
+
+  // FEATURE 3 : tokenId auto-increment
   Counters.Counter private counterTokenIds;
+
+  // FEATURE 4 : borrowing of token
+  //Mapping from token ID to bearer
+  mapping(uint256 => address) private _tokenBearer;
+  // Mapping from bearer to number of beared token
+  mapping (address => Counters.Counter) private _borrowedTokensCount; //TODO not only bearer
+  // Mapping from bearer to list of beared token IDs
+  mapping(address => uint256[]) private _bearedTokens;
+  // Mapping from token ID to index of the bearer tokens list
+  mapping(uint256 => uint256) private _bearedTokensIndex;
+
+  // FEATURE 5 : locking of token borrowing
+  mapping(uint256 => bool) private locked; //array would be very inefficient for this since there is no contains function
 
 
   /**
@@ -39,7 +63,8 @@ contract Thing is
     ERC721Metadata.initialize("Thing", "TG1");
     ERC721Mintable.initialize(minter);
     ERC721Pausable.initialize(pauser);
-    /// do we need another/instead Pausable.initialize(msg.sender) ???
+    /// do we need another/instead
+    Pausable.initialize(pauser);
     Ownable.initialize(owner);
   }
 
@@ -50,27 +75,20 @@ contract Thing is
     ERC721Metadata.initialize("Thing", "TG1");
     ERC721Mintable.initialize(msg.sender);
     ERC721Pausable.initialize(msg.sender);
-    /// do we need another/instead Pausable.initialize(msg.sender) ???
+    /// do we need another/instead
+    Pausable.initialize(msg.sender);
     Ownable.initialize(msg.sender);
   }
 
+  // FEATURE 1 : killable contract
   function kill() public onlyOwner {
     address payable owner = address(uint160(owner()));
     selfdestruct(owner);
   }
 
-  /**
-   * On-chain metadata
-   */
-  //Metadata[] public metadatas;
-  mapping(uint256 => Metadata) private metadatas;
-
-  struct Metadata {
-    string name;
-    string picture;
-    uint256 deposit;
+  function mint() public onlyMinter returns (uint256) {
+    return mint("Test", "QmWzq3Kjxo3zSeS3KRxT6supq9k7ZBRcVGGxAkJmpYtMNC", 0);
   }
-
 
   function mint(string memory name) public onlyMinter returns (uint256) {
     return mint(name, "QmWzq3Kjxo3zSeS3KRxT6supq9k7ZBRcVGGxAkJmpYtMNC", 0);
@@ -96,16 +114,15 @@ contract Thing is
     return newTokenId;
   }
 
-
-
+  // FEATURE 2 : token metadata
   function getTokenMetadata(uint256 tokenId) public view
     returns (uint256 id,
              string memory name,
              string memory picture,
              uint256 deposit,
-             address owner
-             //address bearer,
-             //bool locked
+             address owner,
+             address bearer,
+             bool lock
              ) {
 
             Metadata memory metadata = metadatas[tokenId];
@@ -118,8 +135,139 @@ contract Thing is
             picture = metadata.picture;
             deposit = metadata.deposit;
             owner = ownerOf(tokenId);
-            //bearer = bearerOf(tokenId);
-            //locked = isLocked(tokenId);
+            bearer = bearerOf(tokenId);
+            lock = isLocked(tokenId);
     }
+
+  // FEATURE 4 : borrowing of token
+    /**
+     * @dev Gets the bearer or bearer of the specified token ID.
+     * @param tokenId uint256 ID of the token to query the bearer of
+     * @return address currently marked as the bearer or owner of the given token ID
+     */
+    function bearerOf(uint256 tokenId) public view returns (address) {
+        address owner = ownerOf(tokenId);
+        require(owner != address(0), "Things: owner query for nonexistent token");
+
+        if(_tokenBearer[tokenId] != address(0)) {
+            return _tokenBearer[tokenId];
+        } else {
+            return owner;
+        }
+    }
+
+
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param bearer address to query the balance of
+     * @return uint256 representing the amount beared by the passed address
+     */
+    function bearedBalanceOf(address bearer) public view returns (uint256) {
+        require(bearer != address(0), "Things: balance query for the zero address");
+
+        return _borrowedTokensCount[bearer].current();
+    }
+
+    /**
+     * @dev Private function to add a token to this extension's bearership-tracking data structures.
+     * @param to address representing the new bearer of the given token ID
+     * @param tokenId uint256 ID of the token to be added to the tokens list of the given address
+     */
+    function _addTokenToBearerEnumeration(address to, uint256 tokenId) private {
+        _bearedTokensIndex[tokenId] = _bearedTokens[to].length;
+        _bearedTokens[to].push(tokenId);
+    }
+
+    /**
+     * @dev Private function to remove a token from this extension's ownership-tracking data structures. Note that
+     * while the token is not assigned a new bearer, the _ownedTokensIndex mapping is _not_ updated: this allows for
+     * gas optimizations e.g. when performing a transfer operation (avoiding double writes).
+     * This has O(1) time complexity, but alters the order of the _ownedTokens array.
+     * @param from address representing the previous bearer of the given token ID
+     * @param tokenId uint256 ID of the token to be removed from the tokens list of the given address
+     */
+    function _removeTokenFromBearerEnumeration(address from, uint256 tokenId) private {
+        // To prevent a gap in from's tokens array, we store the last token in the index of the token to delete, and
+        // then delete the last slot (swap and pop).
+
+        uint256 lastTokenIndex = _bearedTokens[from].length.sub(1);
+        uint256 tokenIndex = _bearedTokensIndex[tokenId];
+
+        // When the token to delete is the last token, the swap operation is unnecessary
+        if (tokenIndex != lastTokenIndex) {
+            uint256 lastTokenId = _bearedTokens[from][lastTokenIndex];
+
+            _bearedTokens[from][tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+            _bearedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+        }
+
+        // This also deletes the contents at the last position of the array
+        _bearedTokens[from].length--;
+
+        // Note that _ownedTokensIndex[tokenId] hasn't been cleared: it still points to the old slot (now occupied by
+        // lastTokenId, or just over the end of the array if the token was the last one).
+    }
+
+
+    /**
+     * @dev Transfers the bearing of a given token ID to another address.
+     * Requires the from to be the bearer
+     * @param tokenId uint256 ID of the token to be borrowed
+     */
+    function borrow(uint256 tokenId) public {
+        //TODO require process
+        //require(bearerOf(tokenId) == from, "Things: transfer of token that is not beared");
+        _borrowFrom(bearerOf(tokenId), msg.sender, tokenId);
+    }
+
+    /**
+     * @dev Internal function to borrow a given token ID.
+     * As opposed to borrowFrom, this imposes no restrictions on msg.sender.
+     * @param from current bearer of the token
+     * @param to receipient
+     * @param tokenId uint256 ID of the token to be transferred
+     */
+    function _borrowFrom(address from, address to, uint256 tokenId) internal {
+        require(_exists(tokenId), "Thing: token dont exist");
+        require(to != address(0), "Thing: transfer to the zero address");
+        // FIXME the two next require don't have any messages because there seems to be a strange bug? in oz sdk? I get a "Thing deployment failed with error: Error: Returned error: execution error: undefined"
+        require(!isLocked(tokenId));//, "Thing: token is locked, cant borrow it");
+        require(!paused());//, "Paused");
+
+        address owner = ownerOf(tokenId);
+
+        if(owner != from) {
+            _removeTokenFromBearerEnumeration(from, tokenId);
+            _borrowedTokensCount[from].decrement();
+        }
+        if(owner != to)   {
+            _addTokenToBearerEnumeration(to, tokenId);
+            _borrowedTokensCount[to].increment();
+        }
+
+        _tokenBearer[tokenId] = to;
+
+    }
+
+
+
+    // FEATURE 5 : locking of token borrowing
+    function lockToken(uint256 tokenId) public {
+      require(_exists(tokenId));//, "Thing: token dont exist");
+      //address tokenOwner = ownerOf(tokenId);
+      //require(ownerOf(tokenId) == msg.sender);//, "Thing: only the owner of a token can lock it");
+
+      locked[tokenId] = true;
+    }
+
+    function isLocked(uint256 tokenId) public view returns (bool) {
+        return locked[tokenId];
+    }
+
+    modifier onlyUnlocked(uint256 tokenId) {
+        require(isLocked(tokenId), "Thing: the token is locked");
+        _;
+    }
+
 
 }
--- test/thing.test1.js
+++ test/thing.test1.js
@@ -1,20 +1,21 @@
-const { BN, constants, expectEvent, shouldFail, expectRevert, time } = require('openzeppelin-test-helpers');
+const { BN, constants, expectEvent, shouldFail, expectRevert, time } = require('@openzeppelin/test-helpers');
+//const { accounts, contract } = require('@openzeppelin/test-environment');
 const should = require('chai').should();
+
+//const [ owner2 ] = accounts;
 
 const Thing = artifacts.require('Thing');
 
 contract("thing", async ([_, owner, pauser, minter, ...otherAccounts]) => {
   let thing;
 
+
   beforeEach(async function () {
     thing = await Thing.new();
     thing.initialize(owner, pauser, minter);
+    //await thing.mint("Name", { from: minter });
   });
   
-  it("should have proper owner", async () => {
-    (await thing.owner()).should.equal(owner);
-  });
-
   it('initializes metadata', async function () {
     (await thing.name()).should.equal("Thing");
     (await thing.symbol()).should.equal("TG1");
@@ -29,9 +30,22 @@ contract("thing", async ([_, owner, pauser, minter, ...otherAccounts]) => {
   /**
    * Security test to test basic access control from sensitive functions
    */
+  it("should have a proper owner", async () => {
+    (await thing.owner()).should.equal(owner);
+  });
+
+  /*
+  it("should have a proper owner 2", async () => {
+    const myContract = await Thing.new({ from: owner2 });
+    myContract.initialize({ from: owner2 });
+    expect(await myContract.owner()).to.equal(owner2);
+  });*/
+  
+
   it('protect from unauthorized kill', async function () {
     await expectRevert(
       thing.kill(),
+      //thing.kill({ from: owner }), it works !!!
       'Ownable: caller is not the owner -- Reason given: Ownable: caller is not the owner.'
     );
   });
@@ -45,7 +59,8 @@ contract("thing", async ([_, owner, pauser, minter, ...otherAccounts]) => {
 
   it('protect from illegal minting', async function () {
     await expectRevert(
-      thing.mint("Test"),
+      //thing.methods.mint('Test').send({ from: minter }),
+      thing.mint(),
       'MinterRole: caller does not have the Minter role -- Reason given: MinterRole: caller does not have the Minter role.'
     );
   });
