7fbe8c17b3cc87a05506c8830edae58481c6b911
lildata <lildata@users.noreply.github.com>
2020-01-26 00:31:08 +0100

    shit

--- client/src/components/Deposit/index.js
+++ client/src/components/Deposit/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {useWeb3Injected} from '@openzeppelin/network/lib/react';
 import { ListGroup, Alert } from "react-bootstrap"; 
 
@@ -6,11 +6,14 @@ function Deposit({ jsonInterface }) {
 
   const [contractInstance, setContractInstance] = useState(undefined);
   const [balances, setBalances] = useState(undefined);
+  const [withdrawValue, setWithdrawValue] = useState(undefined);
+  const [fundValue, setFundValue] = useState(undefined);
 
 
   const w3c = useWeb3Injected();
 
   useEffect(() => {
+    //console.log("w3c", w3c);
     //console.log("w3c.networkId", w3c.networkId);
     //console.log("jsonInterface", jsonInterface);
     loadContract(w3c, jsonInterface, w3c.networkId);
@@ -41,6 +44,41 @@ function Deposit({ jsonInterface }) {
   }, [w3c.accounts, contractInstance]);
 
 
+  // should we add? { gas: 50000, gasPrice: 1e6 }  
+  // some nice examples https://github.com/status-im/sticker-market/blob/9bcc786d0f20f16ec2239af359edb0c9e0952659/app/components/erc20token.js
+
+  const fundDeposit = async () => {
+    setFundValue(contractInstance.methods.fundDeposit().send({from: w3c.accounts[0], value: 1000000}));
+  };
+
+  /*  const fundDeposit = async () => {
+    const value = await contractInstance.methods.fundDeposit().send({from: w3c.accounts[0], value: 1000000});
+    setFundValue(value);
+  }; */
+
+  
+  //const fundDeposit = (e) => {
+    //console.log("contractInstance", contractInstance);
+    //e.preventDefault();
+    //var who = e.target.value;
+    //this._addToLog(ERC20Token.options.address+".methods.balanceOf(" + who + ").call()");
+    
+    //contractInstance.methods.fundDeposit().send({from: w3c.accounts[0]}).then(_value => setFundValue(_value))
+  
+    /*var tx = contractInstance.methods.fundDeposit();
+    tx.estimateGas().then((r) => {
+      tx.send({gas: r, from: w3c.accounts[0]});
+    });*/
+  //}
+
+
+  // TODO I have to understand better useCallback
+  const withdrawDeposit = useCallback(() => {
+    setWithdrawValue(contractInstance.methods.withdrawDeposit().send({from: w3c.accounts[0]}));
+  }, [contractInstance]);
+
+
+
   if(!balances) {
     return (
       <Alert key="loading" variant="secondary">Loading...</Alert>
@@ -55,7 +93,9 @@ function Deposit({ jsonInterface }) {
     return (
       <ListGroup>
         <ListGroup.Item>Your balance: {balances.balance}</ListGroup.Item>
+        <ListGroup.Item action onClick={fundDeposit}>Fund deposit (last funded value: {/*fundValue*/})</ListGroup.Item>
         <ListGroup.Item>Your required balance: {balances.requiredBalance} (according to the <a href="/things/borrowed">objects you are borrowing</a>)</ListGroup.Item>
+        <ListGroup.Item action onClick={withdrawDeposit}>Withdraw deposit (last value: {withdrawValue})</ListGroup.Item>      
       </ListGroup>
     );
   }
