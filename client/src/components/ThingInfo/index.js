4aa4f22d1443ef28161d2889bcad589fb18263f8
lildata <lildata@users.noreply.github.com>
2020-01-27 00:00:19 +0100

    Feature off-chain metadata in the UI

--- client/src/components/ThingInfo/index.js
+++ client/src/components/ThingInfo/index.js
@@ -3,6 +3,8 @@ import {useWeb3Injected} from '@openzeppelin/network/lib/react';
 import { ListGroup, Badge } from "react-bootstrap"; 
 import { useParams } from "react-router-dom";
 import EthAddress from "@bit/lil.baseth.eth-address";
+import ipfsClient from "ipfs-http-client";
+
 
 function ThingInfo({ jsonInterface }) {
 
@@ -10,10 +12,17 @@ function ThingInfo({ jsonInterface }) {
 
   const [contractInstance, setContractInstance] = useState(undefined);
   const [onMD, setOnMD] = useState(undefined);
+  const [tokenUri, setTokenUri] = useState(undefined);
+  const [offMD, setOffMD] = useState(undefined);
   const [account, setAccount] = useState(undefined);
   const [change, setChange] = useState(0);
 
   const w3c = useWeb3Injected();
+  const ipfs = ipfsClient({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https"
+  });
 
   useEffect(() => {
     //console.log("w3c.networkId", w3c.networkId);
@@ -40,12 +49,35 @@ function ThingInfo({ jsonInterface }) {
       // FIXME handle error if token does not exist
       setOnMD(await contractInstance.methods.getTokenMetadata(parseInt(tokenId)).call());
     };
+
+    const fetchTokenUri = async () => {
+      // FIXME handle error if token does not exist
+      setTokenUri(await contractInstance.methods.tokenURI(parseInt(tokenId)).call());
+    };
     
     if (w3c && w3c.accounts && w3c.accounts[0]) {
       fetchOnMD();
+      fetchTokenUri();
     }
   
   }, [w3c.accounts, contractInstance, tokenId, change]);
+
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const result = await ipfs.get(tokenUri);
+      if(result[0] && result[0].content) {
+        const json = JSON.parse(result[0].content.toString('utf8'));
+        setOffMD(json);
+      } else {
+        console.error("Something wrong with what was retreived on ipfs", result);
+      }
+    };
+
+    if(tokenUri) fetchData();
+  }, [tokenId, tokenUri]); // empty array because we only run once
+
 
   
   useEffect(() => {
@@ -67,15 +99,26 @@ function ThingInfo({ jsonInterface }) {
     }, 12000);
   }, [contractInstance]);
 
-  if(onMD) {
+  if(onMD && offMD) {
     return (
       <ListGroup>
-  <ListGroup.Item>{onMD.name} <Badge variant="info">{onMD.id}</Badge></ListGroup.Item>
+  <ListGroup.Item><Badge variant="info">{onMD.id}</Badge> {offMD.name}</ListGroup.Item>
+  <ListGroup.Item>{offMD.description}</ListGroup.Item>
+        <ListGroup.Item>Owner: {(account === onMD.owner) ? 'You' : <EthAddress v={onMD.owner} />}</ListGroup.Item>
+        <ListGroup.Item>Bearer: {(account === onMD.bearer) ? 'You' : <EthAddress v={onMD.bearer} />}</ListGroup.Item>
+        <ListGroup.Item>Deposit required: {onMD.deposit} Wei</ListGroup.Item>
+        {(account === onMD.bearer) ? '' : <ListGroup.Item action onClick={actionBorrow}>Borrow</ListGroup.Item>}
+        <ListGroup.Item><img width="200" src={"https://gateway.pinata.cloud/ipfs/"+offMD.image} alt="The object" /></ListGroup.Item>
+      </ListGroup>
+    );
+  } else if(onMD) {
+    return (
+      <ListGroup>
+        <ListGroup.Item>ID {onMD.id}</ListGroup.Item>
         <ListGroup.Item>Owner: {(account === onMD.owner) ? 'You' : <EthAddress v={onMD.owner} />}</ListGroup.Item>
         <ListGroup.Item>Bearer: {(account === onMD.bearer) ? 'You' : <EthAddress v={onMD.bearer} />}</ListGroup.Item>
         <ListGroup.Item>Deposit required: {onMD.deposit}</ListGroup.Item>
         {(account === onMD.bearer) ? '' : <ListGroup.Item action onClick={actionBorrow}>Borrow</ListGroup.Item>}
-        <ListGroup.Item><img src={"https://gateway.pinata.cloud/ipfs/"+onMD.picture} alt="The object" /></ListGroup.Item>
       </ListGroup>
     );
   } else {
