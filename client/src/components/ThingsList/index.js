9283131d03ffde6c3273a75b66e644361df339b1
lildata <lildata@users.noreply.github.com>
2020-01-28 21:16:20 +0100

    Third step (retreive all names from IPFS) for ThingsList :-D

--- client/src/components/ThingsList/index.js
+++ client/src/components/ThingsList/index.js
@@ -41,7 +41,7 @@ function ThingsList({ jsonInterface }) {
   const fetchTotalSupply = async () => {
     let totalSupply = await contractInstance.methods.totalSupply().call({ from: w3c.accounts[0] });
     setTotalSupply(parseInt(totalSupply));
-    console.warn("fetchTotalSupply");
+    //console.warn("fetchTotalSupply");
   };
 
   useEffect(() => {
@@ -61,7 +61,7 @@ function ThingsList({ jsonInterface }) {
   useEffect(() => {
     if (totalSupply) {
       const allTokens = Array(totalSupply).fill().map((_, i) => i);
-      console.log(allTokens);
+      //console.log(allTokens);
 
       // pattern taken from https://flaviocopes.com/javascript-async-await-array-map/
       const functionWithPromise = item => { //a function that returns a promise
@@ -88,25 +88,25 @@ function ThingsList({ jsonInterface }) {
     return (
       <ListGroup>
         <Alert key="info-contract-address" variant="info">Contract address: {contractAddress}</Alert>
+        {tokenIds.map(x => <ListGroup.Item action href={"/things/" + x} key={x}><ThingItem tokenId={x} contractInstance={contractInstance} ipfs={ipfs} /></ListGroup.Item>)}
         <ListGroup.Item>Total supply: {totalSupply}</ListGroup.Item>
-        {tokenIds.map(x => <ListGroup.Item action href={"/things/" + x} key={x}><ThingItem tokenId={x} contractInstance={contractInstance} /></ListGroup.Item>)}
       </ListGroup>
     );
   } else {
     return (
       <ListGroup>
         <Alert key="info-contract-address" variant="info">Contract address: {contractAddress}</Alert>
-
-        <ListGroup.Item>Nothing / Loading</ListGroup.Item>
+        <ListGroup.Item>Nothing / Loading...</ListGroup.Item>
       </ListGroup>
     );
   }
 
 }
 
-function ThingItem({ tokenId, contractInstance }) {
+function ThingItem({ tokenId, contractInstance, ipfs }) {
 
   const [tokenUri, setTokenUri] = useState(undefined);
+  const [name, setName] = useState(undefined);
 
   useEffect(() => {
     if (contractInstance) {
@@ -114,7 +114,23 @@ function ThingItem({ tokenId, contractInstance }) {
     }
   });
 
-  return <div>Object {tokenId} ({(tokenUri) ? tokenUri : 'loading...'})</div>
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const result = await ipfs.get(tokenUri);
+      if (result[0] && result[0].content) {
+        //TODO can we go directly from result to JSON (for perf)
+        const json = JSON.parse(result[0].content.toString('utf8'));
+        setName(json.name);
+      } else {
+        console.error("Something wrong with what was retreived on ipfs", result);
+      }
+    };
+
+    if (tokenUri) fetchData();
+  }, [tokenUri]); 
+
+  return <div>Object {tokenId}: {(name) ? name : 'name loading...'}</div>
 
 }
 
