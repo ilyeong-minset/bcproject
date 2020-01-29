dbbaafbf398cec7e8489782101bdd719ed8b9970
lildata <lildata@users.noreply.github.com>
2020-01-29 12:57:35 +0100

    Progress on Borrowing checks & logic

--- client/src/components/ThingInfo/index.js
+++ client/src/components/ThingInfo/index.js
@@ -4,7 +4,7 @@ import { ListGroup, Badge } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import EthAddress from "../EthAddress/index";
 import ipfsClient from "ipfs-http-client";
-
+import { FaBeer } from 'react-icons/fa';
 
 function ThingInfo({ jsonInterface }) {
 
@@ -16,6 +16,9 @@ function ThingInfo({ jsonInterface }) {
   const [offMD, setOffMD] = useState(undefined);
   const [account, setAccount] = useState(undefined);
   const [change, setChange] = useState(0);
+  const [userDepositBalances, setUserDepositBalances] = useState(undefined);
+  const [missingDeposit, setMissingDeposit] = useState(undefined);
+
 
   const w3c = useWeb3Injected();
   const ipfs = ipfsClient({
@@ -46,7 +49,7 @@ function ThingInfo({ jsonInterface }) {
 
   useEffect(() => {
     const fetchOnMD = async () => {
-      // FIXME handle error if token does not exist
+      // FIXME handle error if token does not exist?
       setOnMD(await contractInstance.methods.getTokenMetadata(parseInt(tokenId)).call());
     };
 
@@ -56,13 +59,27 @@ function ThingInfo({ jsonInterface }) {
       setTokenUri(await contractInstance.methods.tokenURI(parseInt(tokenId)).call());
     };
 
+    const fetchUserDepositBalances = async () => {
+      setUserDepositBalances(await contractInstance.methods.getDepositBalances().call({ from: w3c.accounts[0] }));
+    };
+
     if (w3c && w3c.accounts && w3c.accounts[0] && contractInstance) {
       fetchOnMD();
       fetchTokenUri();
+      fetchUserDepositBalances(); 
     }
 
   }, [w3c.accounts, contractInstance, tokenId, change]);
 
+  useEffect(() => {
+    if(userDepositBalances && onMD && onMD.deposit) {
+      let b = parseInt(userDepositBalances.balance);
+      let rb = parseInt(userDepositBalances.requiredBalance);
+      let rd = parseInt(onMD.deposit);
+      // (350 + 1000) - 650
+      setMissingDeposit((rb + rd) - b);
+    }
+  }, [userDepositBalances, onMD, change]);
 
   useEffect(() => {
 
@@ -85,23 +102,40 @@ function ThingInfo({ jsonInterface }) {
     setAccount(w3c.accounts[0]);
   }, [w3c.accounts]);
 
-  /*useEffect(() => {
-    console.log("metadata",metadata);
-  }, [metadata]);*/
+
+
+  const fundMissingDeposit = async (event) => {
+    event.preventDefault();
+    const tx = await contractInstance.methods.fundDeposit().send({ from: w3c.accounts[0], gasLimit: 500000, value: missingDeposit });
+
+    // trigger an effect x,Y,Z sec later to update values
+    setTimeout(() => {
+      setChange(change + 1);
+    }, 5000);
+    setTimeout(() => {
+      setChange(change + 1);
+    }, 10000);
+    setTimeout(() => {
+      setChange(change + 1);
+    }, 20000);
+  };
+
 
   // TODO I have to understand better useCallback
   const actionBorrow = useCallback(() => {
     //console.log("account", account);
     contractInstance.methods.borrow(parseInt(tokenId)).send({ from: w3c.accounts[0] });
 
-    // trigger an effect 5 sec later to update values
+    // trigger an effect x,Y,Z sec later to update values
     setTimeout(() => {
       setChange(change + 1);
-    }, 5000);
-    // trigger an effect 14 sec later to update values
+    }, 8000);
     setTimeout(() => {
       setChange(change + 1);
-    }, 14000);
+    }, 10000);
+    setTimeout(() => {
+      setChange(change + 1);
+    }, 20000);
   }, [contractInstance]);
 
   if (onMD && offMD) {
@@ -112,7 +146,20 @@ function ThingInfo({ jsonInterface }) {
         <ListGroup.Item>Owner: {(account === onMD.owner) ? 'You' : <EthAddress v={onMD.owner} />}</ListGroup.Item>
         <ListGroup.Item>Bearer: {(account === onMD.bearer) ? 'You' : <EthAddress v={onMD.bearer} />}</ListGroup.Item>
         <ListGroup.Item>Deposit required: {onMD.deposit} Wei</ListGroup.Item>
-        {(account === onMD.bearer) ? '' : <ListGroup.Item action onClick={actionBorrow}>Borrow</ListGroup.Item>}
+        {(onMD.lock) ? <ListGroup.Item>Object is locked ! can't do anything</ListGroup.Item> : ''}
+
+        {((account !== onMD.bearer) && (account === onMD.owner) && (!onMD.lock)) 
+        ? <ListGroup.Item active action onClick={actionBorrow}>Get it back</ListGroup.Item> 
+        : ''}
+
+        {((account !== onMD.bearer) && (missingDeposit <= 0) && (!onMD.lock))
+        ? <ListGroup.Item active action onClick={actionBorrow}>Borrow</ListGroup.Item> 
+        : ''}
+
+        {((account !== onMD.bearer) && (account !== onMD.owner) && (missingDeposit > 0)) 
+        ? <ListGroup.Item active action onClick={fundMissingDeposit}>To borrow this object, your deposit is not enough, fund it ({missingDeposit} Wei)</ListGroup.Item> 
+        : ''}
+
         <ListGroup.Item><img width="200" src={"https://gateway.pinata.cloud/ipfs/" + offMD.image} alt="The object" /></ListGroup.Item>
       </ListGroup>
     );
